db23496edc510556a15ac1841859cc38483453da
Pablo Ajo <pabloajoinglez@gmail.com>
2025-07-21 23:19:26 +0200

    Added Colorama dependancy in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     description='A command line tool for searching through HAR files',
     author='Pablo Ajo',
     packages=find_packages(),
+    install_requires=[
+        'colorama>=0.4.0',
+    ],
     entry_points={
         'console_scripts': [
             'harsearch = harsearch.__main__:main',
